77b5f69b99f9e3815a5cfaab6ddf41fb78a859cf
sschambaher <sebastianschambahermail.com>
2020-11-16 08:39:06 -0500

    first commit

---
+++ app/src/main/java/pe/edu/tecsup/proyectointegrador/MainActivity.java
@@ -0,0 +1,18 @@
+package pe.edu.tecsup.proyectointegrador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //quitar el actionBar por defecto.
+        getSupportActionBar().hide();
+
+    }
+}
